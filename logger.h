6bf6bde34df1ae0b148913a52ca90bc325bc74f4
insert-zkm <zkanylbaev03@gmail.com>
2024-04-08 02:14:58 +0700

    Logger header

--- logger.h
+++ logger.h
@@ -1,14 +1,27 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 
+#include <QFile>
+#include <QObject>
+#include <QString>
 
-class Logger
+#include "file.h"
+#include "filestate.h"
+
+class Logger : QObject
 {
-private:
-    explicit Logger(const Logger&);
-    void operator=(const Logger&);
+Q_OBJECT
+
 public:
-    Logger();
+    Logger() = delete;
+    Logger(const QString& file_name);
+    ~Logger();
+
+    void log(const QString& messege) const;
+
+public slots:
+    void log_file_state(const FileState& fs, const File& file) const;
+
 };
 
 #endif // LOGGER_H
