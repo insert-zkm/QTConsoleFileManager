149ff1d367bb89e40160ea86272f3c41fc893674
insert-zkm <zkanylbaev03@gmail.com>
2024-04-09 04:38:06 +0700

    example of using FileTracker

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <QCoreApplication>
 
 #include <QFile>
-#include <QDir>
-#include <QDebug>
+#include <QTimer>
+#include <QObject>
 
-#include "consolewritter.h"
+#include "logger.h"
 #include "filetracker.h"
 
 int main(int argc, char *argv[])
@@ -17,7 +17,12 @@ int main(int argc, char *argv[])
                    "C:/Users/Zangar/Desktop/Labs/SoftwareDev/Lab2/QTConsoleFileManager/test_files/test_file2.txt"
                });
 
-    fi.start_tracking(500);
+    QTimer timer; 
+    QObject::connect(&timer, &QTimer::timeout, &fi, &FileTracker::check_files);
+    QObject::connect(&fi, &FileTracker::updated, &Logger::get_instance(), &Logger::console_log_file_state);
+    QObject::connect(&fi, &FileTracker::updated, &Logger::get_instance(), &Logger::file_log_file_state);
+
+    timer.start(500);
 
     return a.exec();
 }
