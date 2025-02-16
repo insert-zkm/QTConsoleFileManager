46d88b25a05324f5f6a4f3b32bf5916cde2e56c7
insert-zkm <zkanylbaev03@gmail.com>
2025-02-16 17:17:36 +0700

    feat: heading log usage

--- main.cpp
+++ main.cpp
@@ -13,16 +13,20 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     FileTracker &fi = FileTracker::get_instance();
-            fi.track_file({
-                   PROJECT_PATH "test_files/test_file.txt",
-                   PROJECT_PATH "test_files/test_file1.txt",
-                   PROJECT_PATH "test_files/test_file2.txt"
-               });
+    Logger &lg = Logger::get_instance();
+
+    fi.track_file({
+       PROJECT_PATH "test_files/test_file.txt",
+       PROJECT_PATH "test_files/test_file1.txt",
+       PROJECT_PATH "test_files/test_file2.txt"
+    });
 
     QTimer timer; 
     QObject::connect(&timer, &QTimer::timeout, &fi, &FileTracker::check_files);
-    QObject::connect(&fi, &FileTracker::updated, &Logger::get_instance(), &Logger::console_log_file_state);
+    QObject::connect(&fi, &FileTracker::updated, &lg, &Logger::console_log_file_state);
 
+
+    lg.log_head();
     timer.start(500);
 
     return a.exec();
